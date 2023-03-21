f7cbbc26bcc4697c38c0a3b7655ec62978b8f9df
omarlmadin <omarlmadin95@gmail.com>
2023-03-21 09:13:51 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers together
+ * @a: parametre
+ * @b: parametre
+ * Return: a + b result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
